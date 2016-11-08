709370530514e8995186e54976a2cbe227fe0d76
Varun Rao on Ubuntu <Varun.Rao095@gmail.com>
2016-11-07 23:28:27 -0500

    Made SQL Database, now need to Create AJAX method calls

--- php/welcome.php
+++ php/welcome.php
@@ -1,9 +1,4 @@
 <?php
-	include ('db.php');
-	if(isset($_POST['loginname'])&& isset($_POST['password']))	{
-		$loginname=$_POST['loginname'];
-		$password=$_POST['password'];
-		
-
-	}
+	echo"entered";
+	include_once 'db.php';
 ?>
